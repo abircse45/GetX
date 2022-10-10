1e8c43a4d837f3e85831a62731dd7c1ad7ac73da
abircse45 <abiruodacse@gmail.com>
2022-10-11 00:47:37 +0600

    solved

--- lib/screen/2nd_page.dart
+++ lib/screen/2nd_page.dart
@@ -1,31 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
-import 'package:get/get.dart';
-import '../controller/data_controller.dart';
-class SecondPage extends StatefulWidget {
-  const SecondPage({Key? key}) : super(key: key);
-
-  @override
-  State<SecondPage> createState() => _SecondPageState();
-}
-
-class _SecondPageState extends State<SecondPage> {
-
-  final DataController dataController = Get.put(DataController());
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(onPressed: (){
-          Get.back();
-          // Navigator.pop(context);
-        }, icon: Icon(Icons.arrow_back)),
-        title: Text("Second Page"),
-      ),
-      body: Container(
-        child: Text("${dataController.value}"),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:get/route_manager.dart';
+// import 'package:get/get.dart';
+// import '../controller/data_controller.dart';
+//
+//
+// class SecondPage extends StatelessWidget {
+//   const SecondPage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     final dataController = Get.put(DataController());
+//          return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.purple,
+//
+//         leading: IconButton(onPressed: (){
+//           Get.back();
+//           // Navigator.pop(context);
+//         }, icon: Icon(Icons.arrow_back)),
+//         title: Text("Second Page"),
+//       ),
+//       body: Center(
+//         child: Text("${dataController.value}",style: TextStyle(fontSize: 40),),
+//       ),
+//     );
+//   }
+// }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getx_part1/controller/data_controller.dart';
 import 'package:get/get.dart';
-import 'package:get/route_manager.dart';
-import 'package:getx_part1/screen/2nd_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,44 +10,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final DataController dataController = Get.put(DataController());
 
-  //var   providerData = Provider.of<>(contex,);
+
+  final DataController dataController = Get.put(DataController());
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
         title: Text("GetX"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(
-              () => Text(
-                "${dataController.value}",
-                style: TextStyle(fontSize: 25, color: Colors.black),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                //  Get.off(HomePage());
-
-                ////  Navigator.push(context, MaterialPageRoute(builder: (_)=> SecondPage()));
-                dataController.increment();
-              },
-              child: Text("ADD"),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Get.to(HomePage());
-                },
-                child: Text("Button 2")),
-          ],
-        ),
-      ),
+      body: Obx(() {
+        if (dataController.loader.value) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return ListView.builder(
+            // shrinkWrap: true,
+            // primary: false,
+            itemCount: dataController.getModelList.value.length,
+            itemBuilder: (_, index) {
+              var data= dataController.getModelList.value[index];
+              return Card(
+                elevation: 5,
+                child: ListTile(
+                  title:
+                      Text("${data.title}",style: TextStyle(fontSize: 20,color: Colors.black),),
+                ),
+              );
+            },
+          );
+        }
+      }),
     );
   }
 }
